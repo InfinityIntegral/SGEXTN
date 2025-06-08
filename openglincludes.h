a5e24d0fd7c174ce4019ed95bf27469efb1d1386
Infinity Integral <infinityintegral333@gmail.com>
2025-06-08 19:58:20 -0400

    clear repository, see new README for more info

--- openglincludes.h
+++
@@ -1,9 +0,0 @@
-#ifndef OPENGLINCLUDES_H
-#define OPENGLINCLUDES_H
-
-#include <QtOpenGLWidgets/QOpenGLWidget>
-#include <QtOpenGL/QOpenGLFunctions_4_5_Core>
-#include <QtOpenGL/QOpenGLShaderProgram>
-#include <QtOpenGL/QOpenGLTexture>
-
-#endif // OPENGLINCLUDES_H
--- sgsignalsemitter.cpp
+++
@@ -1,9 +0,0 @@
-#include "sgsignalsemitter.h"
-
-SGSignalsEmitter::SGSignalsEmitter(){
-
-}
-
-void SGSignalsEmitter::sendsignals(){
-    emit (*this).updatescreen();
-}
