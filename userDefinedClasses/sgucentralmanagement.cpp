291d19451de2179240e034edd5704c02e5edaaff
Infinity Integral <infinityintegral333@gmail.com>
2025-06-15 22:19:05 -0400

    added theme colours

--- userDefinedClasses/sgucentralmanagement.cpp
+++ userDefinedClasses/sgucentralmanagement.cpp
@@ -1,5 +1,6 @@
 #include "sgucentralmanagement.h"
 #include <QString>
+#include "../primitives/sgxcolourrgba.h"
 
 // do not forget to replace the icons font file based on the app
 
@@ -7,6 +8,43 @@
 QString SGUCentralManagement::applicationName = "";
 // version number of your application, does not have to be number
 QString SGUCentralManagement::applicationVersion = "";
+
+// set your theme colours
+// SGXColourRGBA SGUCentralManagement::themeColour0 = SGXColourRGBA(0, 0, 0);
+// SGXColourRGBA SGUCentralManagement::themeColour1 = SGXColourRGBA(64, 0, 50);
+SGXColourRGBA SGUCentralManagement::themeColour2 = SGXColourRGBA(128, 0, 100);
+SGXColourRGBA SGUCentralManagement::themeColour3 = SGXColourRGBA(192, 0, 150);
+SGXColourRGBA SGUCentralManagement::themeColour4 = SGXColourRGBA(255, 0, 200);
+SGXColourRGBA SGUCentralManagement::themeColour5 = SGXColourRGBA(255, 64, 214);
+SGXColourRGBA SGUCentralManagement::themeColour6 = SGXColourRGBA(255, 128, 228);
+SGXColourRGBA SGUCentralManagement::themeColour7 = SGXColourRGBA(255, 192, 242);
+SGXColourRGBA SGUCentralManagement::themeColour8 = SGXColourRGBA(255, 255, 255);
+
+// you are recommended to use the theme colours that you defined to set the below colours
+SGXColourRGBA SGUCentralManagement::textBackgroundColour = SGUCentralManagement::themeColour8;
+SGXColourRGBA SGUCentralManagement::textForegroundColour = SGUCentralManagement::themeColour4;
+SGXColourRGBA SGUCentralManagement::buttonBackgroundColour = SGUCentralManagement::themeColour4;
+SGXColourRGBA SGUCentralManagement::buttonForegroundColour = SGUCentralManagement::themeColour8;
+SGXColourRGBA SGUCentralManagement::buttonHoverBackgroundColour = SGUCentralManagement::themeColour3;
+SGXColourRGBA SGUCentralManagement::buttonHoverForegroundColour = SGUCentralManagement::themeColour7;
+SGXColourRGBA SGUCentralManagement::buttonPressedBackgroundColour = SGUCentralManagement::themeColour2;
+SGXColourRGBA SGUCentralManagement::buttonPressedForegroundColour = SGUCentralManagement::themeColour6;
+SGXColourRGBA SGUCentralManagement::unselectedButtonBackgroundColour = SGUCentralManagement::themeColour5;
+SGXColourRGBA SGUCentralManagement::unselectedButtonForegroundColour = SGUCentralManagement::themeColour8;
+SGXColourRGBA SGUCentralManagement::unselectedButtonHoverBackgroundColour = SGUCentralManagement::themeColour4;
+SGXColourRGBA SGUCentralManagement::unselectedButtonHoverForegroundColour = SGUCentralManagement::themeColour7;
+SGXColourRGBA SGUCentralManagement::inputFieldBackgroundColour = SGUCentralManagement::themeColour4;
+SGXColourRGBA SGUCentralManagement::inputFieldForegroundColour = SGUCentralManagement::themeColour8;
+SGXColourRGBA SGUCentralManagement::inputFieldSelectedHighlightBackgroundColour = SGUCentralManagement::themeColour6;
+SGXColourRGBA SGUCentralManagement::inputFieldSelectedHighlightForegroundColour = SGUCentralManagement::themeColour2;
+SGXColourRGBA SGUCentralManagement::inputFieldHoverBackgroundColour = SGUCentralManagement::themeColour3;
+SGXColourRGBA SGUCentralManagement::inputFieldHoverForegroundColour = SGUCentralManagement::themeColour7;
+SGXColourRGBA SGUCentralManagement::inputFieldFocusedBackgroundColour = SGUCentralManagement::themeColour2;
+SGXColourRGBA SGUCentralManagement::inputFieldFocusedForegroundColour = SGUCentralManagement::themeColour6;
+SGXColourRGBA SGUCentralManagement::scrollBarBackgroundColour = SGUCentralManagement::themeColour6;
+SGXColourRGBA SGUCentralManagement::scrollBarForegroundColour = SGUCentralManagement::themeColour4;
+SGXColourRGBA SGUCentralManagement::scrollBarForegroundHoverColour = SGUCentralManagement::themeColour3;
+SGXColourRGBA SGUCentralManagement::scrollBarForegroundPressedColour = SGUCentralManagement::themeColour2;
 
 // is run when app opens
 void SGUCentralManagement::initialise(){
--- userDefinedClasses/sgucentralmanagement.h
+++ userDefinedClasses/sgucentralmanagement.h
@@ -2,6 +2,7 @@
 #define SGUCENTRALMANAGEMENT_H
 
 #include <QObject>
+#include "../primitives/sgxcolourrgba.h"
 
 class SGUCentralManagement : public QObject // non instantiable class for essential application data that should be define by the user, including application name, theme colours, custom initialisation and custom termination
 {
@@ -17,6 +18,40 @@ public:
     static void terminate(); // temination function run when app closes
     static QString applicationName; // name of application displayed
     static QString applicationVersion; // version number of application
+    static SGXColourRGBA rootWindowColour; // colour of root window, can be translucent
+    // static SGXColourRGBA themeColour0; // theme colour 0
+    // static SGXColourRGBA themeColour1; // theme colour 1
+    static SGXColourRGBA themeColour2; // theme colour 2
+    static SGXColourRGBA themeColour3; // theme colour 3
+    static SGXColourRGBA themeColour4; // theme colour 4
+    static SGXColourRGBA themeColour5; // theme colour 5
+    static SGXColourRGBA themeColour6; // theme colour 6
+    static SGXColourRGBA themeColour7; // theme colour 7
+    static SGXColourRGBA themeColour8; // theme colour 8
+    static SGXColourRGBA textBackgroundColour; // background colour of text labels
+    static SGXColourRGBA textForegroundColour; // foreground colour of text labels
+    static SGXColourRGBA buttonBackgroundColour; // background colour of button
+    static SGXColourRGBA buttonForegroundColour; // foreground colour of button
+    static SGXColourRGBA buttonHoverBackgroundColour; // background colour when cursor is hovering over button
+    static SGXColourRGBA buttonHoverForegroundColour; // foreground colour when cursor is hovering over button
+    static SGXColourRGBA buttonPressedBackgroundColour; // background colour when button is being pressed
+    static SGXColourRGBA buttonPressedForegroundColour; // foreground colour when button is being pressed
+    static SGXColourRGBA unselectedButtonBackgroundColour; // background colour of button representing unselected options
+    static SGXColourRGBA unselectedButtonForegroundColour; // foreground colour of button representing unselected options
+    static SGXColourRGBA unselectedButtonHoverBackgroundColour; // background colour of button representing unselected options when being hovered over
+    static SGXColourRGBA unselectedButtonHoverForegroundColour; // foreground colour of button representing unselected options when being hovered over
+    static SGXColourRGBA inputFieldBackgroundColour; // background colour of input field
+    static SGXColourRGBA inputFieldForegroundColour; // foreground colour of input field
+    static SGXColourRGBA inputFieldSelectedHighlightBackgroundColour; // background colour of input field selected text
+    static SGXColourRGBA inputFieldSelectedHighlightForegroundColour; // foreground colour of input field selected text
+    static SGXColourRGBA inputFieldHoverBackgroundColour; // background colour of input field when hovered over
+    static SGXColourRGBA inputFieldHoverForegroundColour; // foreground colour of input field when hovered over
+    static SGXColourRGBA inputFieldFocusedBackgroundColour; // background colour of input field when focused
+    static SGXColourRGBA inputFieldFocusedForegroundColour; // foreground colour of input field when focused
+    static SGXColourRGBA scrollBarBackgroundColour; // background colour of scroll bar
+    static SGXColourRGBA scrollBarForegroundColour; // foreground colour of scroll bar, applied to scroll bar handle
+    static SGXColourRGBA scrollBarForegroundHoverColour; // foreground colour of scroll bar, applied to scroll bar handle when scroll bar is hovered over
+    static SGXColourRGBA scrollBarForegroundPressedColour; // foreground colour of scroll bar, applied to scroll bar handle when scroll bar is pressed
 };
 
 #endif // SGUCENTRALMANAGEMENT_H
