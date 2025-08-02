a14d28b63221a2aa5cbc61208029ebfb17e6e709
Infinity Integral <infinityintegral333@gmail.com>
2025-08-02 10:53:42 +0800

    fixed locale

--- SGEXTN/misc/main.cpp
+++ SGEXTN/misc/main.cpp
@@ -4,9 +4,11 @@
 #include <QGuiApplication>
 #include "../quickui/sgxquickuiinterface.h"
 #include <QQuickStyle>
+#include <QLocale>
 
 int main(int argc, char** argv){ // NOLINT(misc-include-cleaner)
     QGuiApplication app(argc, argv); // NOLINT(misc-const-correctness)
+    QLocale::setDefault(QLocale(QLocale::English, QLocale::Singapore));
     QQuickStyle::setStyle("Basic");
     QQmlApplicationEngine e;
     SGXQuickUIInterface::e = &e;
