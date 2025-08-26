1145ed434b4590987fd3bcd81157049758c2daa1
unknown <infinityintegral333@gmail.com>
2025-08-26 08:58:39 +0800

    committing from Singapore - removed comments from customisation pages

--- SGEXTN/customisation/sgxthemecolourscustomisation.h
+++ SGEXTN/customisation/sgxthemecolourscustomisation.h
@@ -4,18 +4,13 @@
 #include <QObject>
 #include <qtmetamacros.h>
 
-class SGXThemeColoursCustomisation : public QObject // non instantiable class for customising app theme colours
+class SGXThemeColoursCustomisation : public QObject
 {
     Q_OBJECT
 public:
     SGXThemeColoursCustomisation() = delete;
-    static void loadThemeColours(); // load theme colours from file
-    // note that during the National Day period, the theme will be changed to "For Our Nation" instead of "Standard Light" (the default theme), this overrides custom theme configuration
-    // there is no builtin API for themes, but all 05524F apps using SGEXTN should have the following themes
-    // Standard Light: mainly light pink, default theme
-    // Standard Dark: mainly dark pink, equivalent to dark mode
-    // For Our Nation: uses the same hue of red as our flag, default during National Day period
-    static void syncThemeColours(); // write current theme colours to file
+    static void loadThemeColours();
+    static void syncThemeColours();
 };
 
 #endif // SGXTHEMECOLOURSCUSTOMISATION_H
--- SGEXTN/customisation/sgxvesiclespropertiescustomisation.h
+++ SGEXTN/customisation/sgxvesiclespropertiescustomisation.h
@@ -9,9 +9,8 @@ class SGXVesiclesPropertiesCustomisation : public QObject
     Q_OBJECT
 public:
     SGXVesiclesPropertiesCustomisation() = delete;
-    static void loadVesicleProperties(); // load vesicle properties from file
-    // note that during the National Day period, the vesicles will use the same colour as the red on our flag for their membranes and the same shade of white for their contents, because vesicles need to wear red and white to celebrate also
-    static void syncVesicleProperties(); // write current vesicle properties to file
+    static void loadVesicleProperties();
+    static void syncVesicleProperties();
 };
 
 #endif // SGXVESICLESPROPERTIESCUSTOMISATION_H
