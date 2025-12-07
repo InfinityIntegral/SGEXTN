496d9b73656431a3545a3e1b774a9d3a77605ae4
Infinity Integral (05524F) <infinityintegral333@gmail.com>
2025-12-07 12:13:30 +0800

    added icon

--- internal_test/src/main.cpp
+++ internal_test/src/main.cpp
@@ -69,6 +69,7 @@ void init(){
     SGXCentral::organisationName = "05524F (Singapore)";
     SGXCentral::customInitialise = &test;
     SGXCentral::folderName = "SGEXTN";
+    SGXCentral::pathToAppIcon = ":/SGEXTN/SGEXTN_icon.png";
 }
 }
 
