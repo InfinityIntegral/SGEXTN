d889adab17086562e4a1f0f6f3039399dbadbf33
Infinity Integral (05524F) <infinityintegral333@gmail.com>
2025-12-30 09:56:54 +0800

    modified SGXVector2 API to ensure symmetry of arguments

--- SG_Core/include/SGXVector2.h
+++ SG_Core/include/SGXVector2.h
@@ -32,10 +32,10 @@ public:
     [[nodiscard]] float getMagnitudeSquare() const;
     SGXVector2& normalise();
     SGXVector2& normaliseGivenMagnitude(float m);
-    [[nodiscard]] float getDistance(SGXVector2 x) const;
-    [[nodiscard]] float getDistanceSquare(SGXVector2 x) const;
+    static float getDistance(SGXVector2 a, SGXVector2 b);
+    static float getDistanceSquare(SGXVector2 a, SGXVector2 b);
     [[nodiscard]] float getArgument() const;
-    [[nodiscard]] float getAngleBetween(SGXVector2 x) const;
+    static float getAngleBetween(SGXVector2 a, SGXVector2 b);
     SGXVector2& redirectUsingArgument(float a);
     SGXVector2& invert();
     SGXVector2& rotateClockwise90();
@@ -43,11 +43,11 @@ public:
     SGXVector2& rotate180();
     SGXVector2& rotateClockwise(float a);
     SGXVector2& rotateCounterclockwise(float a);
-    [[nodiscard]] bool isCollinear(SGXVector2 a, SGXVector2 b, float limit) const;
-    [[nodiscard]] bool isParallel(SGXVector2 a2, SGXVector2 b1, SGXVector2 b2, float limit) const;
-    [[nodiscard]] bool isPerpendicular(SGXVector2 a, SGXVector2 b, float limit) const;
-    [[nodiscard]] SGXVector2 midpoint(SGXVector2 x) const;
-    [[nodiscard]] SGXVector2 linearInterpolate(SGXVector2 x, float f) const;
+    static bool isCollinear(SGXVector2 x, SGXVector2 a, SGXVector2 b, float limit);
+    static bool isParallel(SGXVector2 a1, SGXVector2 a2, SGXVector2 b1, SGXVector2 b2, float limit);
+    static bool isPerpendicular(SGXVector2 x, SGXVector2 a, SGXVector2 b, float limit);
+    static SGXVector2 midpoint(SGXVector2 a, SGXVector2 b);
+    static SGXVector2 linearInterpolate(SGXVector2 a, SGXVector2 b, float f);
     SGXVector2& reflectAcrossX();
     SGXVector2& reflectAcrossY();
     SGXVector2& reflectAcrossPoint(SGXVector2 x);
@@ -61,8 +61,8 @@ public:
     [[nodiscard]] SGXVector2 getNearestPointOnLine(SGXVector2 a, SGXVector2 b) const;
     [[nodiscard]] SGXVector2 getNearestPointOnSegment(SGXVector2 a, SGXVector2 b) const;
     [[nodiscard]] SGXVector2 getNearestPointOnCircle(float a, float b, float r) const;
-    [[nodiscard]] float dotProduct(SGXVector2 x) const;
-    [[nodiscard]] float crossProduct(SGXVector2 x) const;
+    static float dotProduct(SGXVector2 a, SGXVector2 b);
+    static float crossProduct(SGXVector2 a, SGXVector2 b);
 };
 
 #endif // SGXVECTOR2_H
