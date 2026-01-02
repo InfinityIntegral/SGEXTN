6e81419ac76cf29d1c75fd209c1d3def89a6b788
Infinity Integral (05524F) <infinityintegral333@gmail.com>
2026-01-02 20:34:11 +0800

    removed undefined behaviour from SG_RI module

--- SG_RI/src/SGRElementBufferObject.cpp
+++ SG_RI/src/SGRElementBufferObject.cpp
@@ -3,8 +3,11 @@
 #include <SGRBaseRenderer.h>
 #include <private_api_RI/SGRRendererNode.h>
 #include <SGRDataBuffer.h>
+#include <private_api_Containers/SGLCrash.h>
 
 SGRElementBufferObject::SGRElementBufferObject(SGRBaseRenderer *renderControl, int bufferSize) : SGRDataBuffer(){
+    if(bufferSize < 0){SGLCrash::crash("SGRElementBufferObject constructor crashed as the size is negative");}
+    if(renderControl == nullptr){SGLCrash::crash("SGRElementBufferObject constructor crashed as the associated render control object is nullptr");}
     (*this).rhi = (*(*renderControl).node).rhi;
     (*this).data = (*rhi).newBuffer(QRhiBuffer::Dynamic, QRhiBuffer::IndexBuffer, bufferSize);
     (*(*this).data).create();
