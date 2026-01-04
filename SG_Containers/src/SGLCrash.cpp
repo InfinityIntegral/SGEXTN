05c1299298a417496acd26c0dec14adae63ce856
Infinity Integral (05524F) <infinityintegral333@gmail.com>
2026-01-04 23:54:06 +0800

    add crash logs

--- SG_Containers/src/SGLCrash.cpp
+++ SG_Containers/src/SGLCrash.cpp
@@ -2,8 +2,11 @@
 #include <stdexcept>
 #include <string>
 
+void (*SGLCrash::logCrashMessage)(const char*) = nullptr;
+
 void SGLCrash::crash(const char *msg){
     std::string message(msg);
     message = std::string("\nERROR\n") + message + "\nSGEXTN framework has detected misuse of API and has crashed to prevent undefined behaviour\nPls read the documentation to see how to properly use the mentioned function\nRun the app with a debugger by clicking the button below the run button for a stack trace";
+    if(SGLCrash::logCrashMessage != nullptr){SGLCrash::logCrashMessage(message.c_str());}
     throw std::runtime_error(message);
 }
