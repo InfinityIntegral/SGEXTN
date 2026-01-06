27b51fa6791ca4161310a74ccb00b414ccefb442
Infinity Integral (05524F) <infinityintegral333@gmail.com>
2026-01-06 20:44:30 +0800

    fixed colours of SGEXTN provided UI

--- SG_Widgets/src/SGWBackground.cpp
+++ SG_Widgets/src/SGWBackground.cpp
@@ -12,7 +12,7 @@ SGLStack<SGWBackground*>* SGWBackground::activePages = nullptr;
 
 SGWBackground::SGWBackground(SGWWidget *parent, float x1, float x0, float y1, float y0, float w1, float w0, float h1, float h0) : SGWWidget(parent, x1, x0, y1, y0, w1, w0, h1, h0){
     (*this).usingTheme = true;
-    (*this).themeColour = 8;
+    (*this).themeColour = 3;
     (*this).colour = SGXThemeColours::getThemeColour(3);
 }
 
