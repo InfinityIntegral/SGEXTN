c81d60cbf80b87e7c6c428c4e84f1155c83aabc1
Infinity Integral (05524F) <infinityintegral333@gmail.com>
2026-06-26 09:04:42 +0800

    wrote normal distribution and related distributions

---
+++ SeerattraNum/include/SGEXTN/SeerattraNum/CauchyDistribution.h
@@ -0,0 +1,44 @@
+/*
+   Copyright 2026 05524F.sg
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+// BuildLah license check: SGEXTN 7.0.0
+
+#pragma once
+#include <SGEXTN/Containers/Array.h>
+
+namespace SGEXTN {
+namespace SeerattraNum {
+template <typename FloatingPoint> class BuildLah_SGEXTN_SeerattraNum CauchyDistribution {
+public:
+    void* private_stlRandomEngine;
+    void* private_stlDistribution;
+    FloatingPoint private_median;
+    FloatingPoint private_halfWidth;
+    CauchyDistribution(bool useGlobal, FloatingPoint median, FloatingPoint halfWidth);
+    CauchyDistribution(const CauchyDistribution&) = delete;
+    CauchyDistribution& operator=(const CauchyDistribution&) = delete;
+    CauchyDistribution(CauchyDistribution&&) = delete;
+    CauchyDistribution& operator=(CauchyDistribution&&) = delete;
+    ~CauchyDistribution();
+    void seed(const SGEXTN::Containers::Array<unsigned int>& seedArray);
+    [[nodiscard]] FloatingPoint randomValue();
+    [[nodiscard]] SGEXTN::Containers::Array<FloatingPoint> randomValueArray(int count);
+    [[nodiscard]] FloatingPoint getMedian() const;
+    [[nodiscard]] FloatingPoint getHalfWidth() const;
+    void setMedian(FloatingPoint median);
+    void setHalfWidth(FloatingPoint halfWidth);
+};
+}
+}
