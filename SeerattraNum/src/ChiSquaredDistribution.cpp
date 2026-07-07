370512bb7b6f65dac1cdf2229e44f34254940da9
Infinity Integral (05524F) <infinityintegral333@gmail.com>
2026-07-07 20:34:18 +0800

    rewrote ChiSquaredDistribution

--- SeerattraNum/src/ChiSquaredDistribution.cpp
+++ SeerattraNum/src/ChiSquaredDistribution.cpp
@@ -16,54 +16,48 @@
 // BuildLah license check: SGEXTN 7.0.0
 
 #include <SGEXTN/SeerattraNum/ChiSquaredDistribution.h>
-#include <SGEXTN/SeerattraNum/private_api/UnsafeCasts.h>
 #include <SGEXTN/Containers/Array.h>
 #include <SGEXTN/Containers/ForceCrash.h>
-#include <SGEXTN/SeerattraNum/SimpleRandom.h>
-#include <random>
+#include <SGEXTN/SeerattraNum/DirectRandom.h>
 
-template <typename FloatingPoint> SGEXTN::SeerattraNum::ChiSquaredDistribution<FloatingPoint>::ChiSquaredDistribution(bool useGlobal, FloatingPoint degreesOfFreedom){
+SGEXTN::SeerattraNum::ChiSquaredDistribution::ChiSquaredDistribution(bool useGlobal, float degreesOfFreedom) : private_degreesOfFreedom(degreesOfFreedom), private_rng(nullptr), private_ownsRng(useGlobal == false), private_gammaDistribution(true, 1.0f, 2.0f){
     if(degreesOfFreedom <= 0.0){SGEXTN_IMMEDIATE_CRASH("SGEXTN::SeerattraNum::ChiSquaredDistribution constructor crashed because requested number of degrees of freedom is nonpositive");}
-    private_degreesOfFreedom = degreesOfFreedom;
-    private_stlRandomEngine = SGEXTN::SeerattraNum::SimpleRandom::private_createRandomEngine(useGlobal);
-    private_stlDistribution = SGEXTN::SeerattraNum::UnsafeCasts<std::chi_squared_distribution<FloatingPoint>>::eraseType(new std::chi_squared_distribution<FloatingPoint>(degreesOfFreedom));
+    private_rng = SGEXTN::SeerattraNum::DirectRandom::private_createRng(useGlobal);
+    private_gammaDistribution.private_rng = private_rng;
+    private_gammaDistribution.private_standardNormalDistribution.private_rng = private_rng;
+    private_gammaDistribution.setVariableCount(0.5f * degreesOfFreedom);
 }
 
-template <typename FloatingPoint> SGEXTN::SeerattraNum::ChiSquaredDistribution<FloatingPoint>::~ChiSquaredDistribution(){
-    delete SGEXTN::SeerattraNum::UnsafeCasts<std::mt19937_64>::uneraseType(private_stlRandomEngine);
-    delete SGEXTN::SeerattraNum::UnsafeCasts<std::chi_squared_distribution<FloatingPoint>>::uneraseType(private_stlDistribution);
+SGEXTN::SeerattraNum::ChiSquaredDistribution::~ChiSquaredDistribution(){
+    if(private_ownsRng == true){delete private_rng;}
 }
 
-template <typename FloatingPoint> void SGEXTN::SeerattraNum::ChiSquaredDistribution<FloatingPoint>::seed(const SGEXTN::Containers::Array<unsigned int>& seedArray){
-    if(private_stlRandomEngine == nullptr){SGEXTN_IMMEDIATE_CRASH("SGEXTN::SeerattraNum::ChiSquaredDistribution::seed crashed because cannot seed global rng");}
-    SGEXTN::SeerattraNum::SimpleRandom::private_seedRandomEngine(private_stlRandomEngine, seedArray);
+void SGEXTN::SeerattraNum::ChiSquaredDistribution::seed(const SGEXTN::Containers::Array<unsigned int>& seedArray){
+    if(private_ownsRng == false){SGEXTN_IMMEDIATE_CRASH("SGEXTN::SeerattraNum::ChiSquaredDistribution::seed crashed because cannot seed global rng");}
+    SGEXTN::SeerattraNum::DirectRandom* temp = private_rng;
+    private_rng = temp;
+    (*private_rng).seed(seedArray);
 }
 
-template <typename FloatingPoint> FloatingPoint SGEXTN::SeerattraNum::ChiSquaredDistribution<FloatingPoint>::randomValue(){
-    void* randomEngine = private_stlRandomEngine;
-    if(randomEngine == nullptr){randomEngine = SGEXTN::SeerattraNum::SimpleRandom::private_getRandomEngine();}
-    return ((*SGEXTN::SeerattraNum::UnsafeCasts<std::chi_squared_distribution<FloatingPoint>>::uneraseType(private_stlDistribution))(*SGEXTN::SeerattraNum::UnsafeCasts<std::mt19937_64>::uneraseType(randomEngine)));
+float SGEXTN::SeerattraNum::ChiSquaredDistribution::randomValue(){
+    return private_gammaDistribution.randomValue();
 }
 
-template <typename FloatingPoint> SGEXTN::Containers::Array<FloatingPoint> SGEXTN::SeerattraNum::ChiSquaredDistribution<FloatingPoint>::randomValueArray(int count){
+SGEXTN::Containers::Array<float> SGEXTN::SeerattraNum::ChiSquaredDistribution::randomValueArray(int count){
     if(count < 0){SGEXTN_IMMEDIATE_CRASH("SGEXTN::SeerattraNum::ChiSquaredDistribution::randomValueArray crashed because a negative number of outputs is requested");}
-    SGEXTN::Containers::Array<FloatingPoint> outputArray(count);
+    SGEXTN::Containers::Array<float> outputArray(count);
     for(int i=0; i<count; i++){
         outputArray.at(i) = randomValue();
     }
     return outputArray;
 }
 
-template <typename FloatingPoint> FloatingPoint SGEXTN::SeerattraNum::ChiSquaredDistribution<FloatingPoint>::getDegreesOfFreedom() const {
+float SGEXTN::SeerattraNum::ChiSquaredDistribution::getDegreesOfFreedom() const {
     return private_degreesOfFreedom;
 }
 
-template <typename FloatingPoint> void SGEXTN::SeerattraNum::ChiSquaredDistribution<FloatingPoint>::setDegreesOfFreedom(FloatingPoint degreesOfFreedom){
+void SGEXTN::SeerattraNum::ChiSquaredDistribution::setDegreesOfFreedom(float degreesOfFreedom){
     if(degreesOfFreedom <= 0.0){SGEXTN_IMMEDIATE_CRASH("SGEXTN::SeerattraNum::ChiSquaredDistribution::setDegreesOfFreedom crashed because requested number of degrees of freedom is nonpositive");}
     private_degreesOfFreedom = degreesOfFreedom;
-    delete SGEXTN::SeerattraNum::UnsafeCasts<std::chi_squared_distribution<FloatingPoint>>::uneraseType(private_stlDistribution);
-    private_stlDistribution = SGEXTN::SeerattraNum::UnsafeCasts<std::chi_squared_distribution<FloatingPoint>>::eraseType(new std::chi_squared_distribution<FloatingPoint>(degreesOfFreedom));
+    private_gammaDistribution.setVariableCount(0.5f * degreesOfFreedom);
 }
-
-template class SGEXTN::SeerattraNum::ChiSquaredDistribution<float>;
-template class SGEXTN::SeerattraNum::ChiSquaredDistribution<double>;
