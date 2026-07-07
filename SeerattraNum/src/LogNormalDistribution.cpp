2ce1a76944c974c505ec015dd6b73eea12406fc9
Infinity Integral (05524F) <infinityintegral333@gmail.com>
2026-07-07 20:03:45 +0800

    rewrote LogNormalDistribution

--- SeerattraNum/src/LogNormalDistribution.cpp
+++ SeerattraNum/src/LogNormalDistribution.cpp
@@ -16,65 +16,58 @@
 // BuildLah license check: SGEXTN 7.0.0
 
 #include <SGEXTN/SeerattraNum/LogNormalDistribution.h>
-#include <SGEXTN/SeerattraNum/private_api/UnsafeCasts.h>
 #include <SGEXTN/Containers/Array.h>
 #include <SGEXTN/Containers/ForceCrash.h>
-#include <SGEXTN/SeerattraNum/SimpleRandom.h>
-#include <random>
+#include <SGEXTN/SeerattraNum/DirectRandom.h>
+#include <SGEXTN/Math/FloatMath.h>
 
-template <typename FloatingPoint> SGEXTN::SeerattraNum::LogNormalDistribution<FloatingPoint>::LogNormalDistribution(bool useGlobal, FloatingPoint meanOfLn, FloatingPoint standardDeviationOfLn){
+SGEXTN::SeerattraNum::LogNormalDistribution::LogNormalDistribution(bool useGlobal, float meanOfLn, float standardDeviationOfLn) : private_meanOfLn(meanOfLn), private_standardDeviationOfLn(standardDeviationOfLn), private_rng(nullptr), private_ownsRng(useGlobal == false), private_normalDistribution(true, 0.0f, 1.0f){
     if(standardDeviationOfLn <= 0.0){SGEXTN_IMMEDIATE_CRASH("SGEXTN::SeerattraNum::LogNormalDistribution constructor crashed because requested standard deviation is nonpositive");}
-    private_meanOfLn = meanOfLn;
-    private_standardDeviationOfLn = standardDeviationOfLn;
-    private_stlRandomEngine = SGEXTN::SeerattraNum::SimpleRandom::private_createRandomEngine(useGlobal);
-    private_stlDistribution = SGEXTN::SeerattraNum::UnsafeCasts<std::lognormal_distribution<FloatingPoint>>::eraseType(new std::lognormal_distribution<FloatingPoint>(meanOfLn, standardDeviationOfLn));
+    private_rng = SGEXTN::SeerattraNum::DirectRandom::private_createRng(useGlobal);
+    private_normalDistribution.private_rng = private_rng;
+    private_normalDistribution.setMean(meanOfLn);
+    private_normalDistribution.setStandardDeviation(standardDeviationOfLn);
 }
 
-template <typename FloatingPoint> SGEXTN::SeerattraNum::LogNormalDistribution<FloatingPoint>::~LogNormalDistribution(){
-    delete SGEXTN::SeerattraNum::UnsafeCasts<std::mt19937_64>::uneraseType(private_stlRandomEngine);
-    delete SGEXTN::SeerattraNum::UnsafeCasts<std::lognormal_distribution<FloatingPoint>>::uneraseType(private_stlDistribution);
+SGEXTN::SeerattraNum::LogNormalDistribution::~LogNormalDistribution(){
+    if(private_ownsRng == true){delete private_rng;}
 }
 
-template <typename FloatingPoint> void SGEXTN::SeerattraNum::LogNormalDistribution<FloatingPoint>::seed(const SGEXTN::Containers::Array<unsigned int>& seedArray){
-    if(private_stlRandomEngine == nullptr){SGEXTN_IMMEDIATE_CRASH("SGEXTN::SeerattraNum::LogNormalDistribution::seed crashed because cannot seed global rng");}
-    SGEXTN::SeerattraNum::SimpleRandom::private_seedRandomEngine(private_stlRandomEngine, seedArray);
+void SGEXTN::SeerattraNum::LogNormalDistribution::seed(const SGEXTN::Containers::Array<unsigned int>& seedArray){
+    if(private_ownsRng == false){SGEXTN_IMMEDIATE_CRASH("SGEXTN::SeerattraNum::LogNormalDistribution::seed crashed because cannot seed global rng");}
+    SGEXTN::SeerattraNum::DirectRandom* temp = private_rng;
+    private_rng = temp;
+    (*private_rng).seed(seedArray);
 }
 
-template <typename FloatingPoint> FloatingPoint SGEXTN::SeerattraNum::LogNormalDistribution<FloatingPoint>::randomValue(){
-    void* randomEngine = private_stlRandomEngine;
-    if(randomEngine == nullptr){randomEngine = SGEXTN::SeerattraNum::SimpleRandom::private_getRandomEngine();}
-    return ((*SGEXTN::SeerattraNum::UnsafeCasts<std::lognormal_distribution<FloatingPoint>>::uneraseType(private_stlDistribution))(*SGEXTN::SeerattraNum::UnsafeCasts<std::mt19937_64>::uneraseType(randomEngine)));
+float SGEXTN::SeerattraNum::LogNormalDistribution::randomValue(){
+    return SGEXTN::Math::FloatMath<float>::powerOfE(private_normalDistribution.randomValue());
 }
 
-template <typename FloatingPoint> SGEXTN::Containers::Array<FloatingPoint> SGEXTN::SeerattraNum::LogNormalDistribution<FloatingPoint>::randomValueArray(int count){
+SGEXTN::Containers::Array<float> SGEXTN::SeerattraNum::LogNormalDistribution::randomValueArray(int count){
     if(count < 0){SGEXTN_IMMEDIATE_CRASH("SGEXTN::SeerattraNum::LogNormalDistribution::randomValueArray crashed because a negative number of outputs is requested");}
-    SGEXTN::Containers::Array<FloatingPoint> outputArray(count);
+    SGEXTN::Containers::Array<float> outputArray(count);
     for(int i=0; i<count; i++){
         outputArray.at(i) = randomValue();
     }
     return outputArray;
 }
 
-template <typename FloatingPoint> FloatingPoint SGEXTN::SeerattraNum::LogNormalDistribution<FloatingPoint>::getMeanOfLn() const {
+float SGEXTN::SeerattraNum::LogNormalDistribution::getMeanOfLn() const {
     return private_meanOfLn;
 }
 
-template <typename FloatingPoint> FloatingPoint SGEXTN::SeerattraNum::LogNormalDistribution<FloatingPoint>::getStandardDeviationOfLn() const {
+float SGEXTN::SeerattraNum::LogNormalDistribution::getStandardDeviationOfLn() const {
     return private_standardDeviationOfLn;
 }
 
-template <typename FloatingPoint> void SGEXTN::SeerattraNum::LogNormalDistribution<FloatingPoint>::setMeanOfLn(FloatingPoint meanOfLn){
+void SGEXTN::SeerattraNum::LogNormalDistribution::setMeanOfLn(float meanOfLn){
     private_meanOfLn = meanOfLn;
-    delete SGEXTN::SeerattraNum::UnsafeCasts<std::lognormal_distribution<FloatingPoint>>::uneraseType(private_stlDistribution);
-    private_stlDistribution = SGEXTN::SeerattraNum::UnsafeCasts<std::lognormal_distribution<FloatingPoint>>::eraseType(new std::lognormal_distribution<FloatingPoint>(meanOfLn, private_standardDeviationOfLn));
+    private_normalDistribution.setMean(meanOfLn);
 }
 
-template <typename FloatingPoint> void SGEXTN::SeerattraNum::LogNormalDistribution<FloatingPoint>::setStandardDeviationOfLn(FloatingPoint standardDeviationOfLn){
+void SGEXTN::SeerattraNum::LogNormalDistribution::setStandardDeviationOfLn(float standardDeviationOfLn){
     if(standardDeviationOfLn <= 0.0){SGEXTN_IMMEDIATE_CRASH("SGEXTN::SeerattraNum::LogNormalDistribution::setStandardDeviationOfLn crashed because requested standard deviation is nonpositive");}
     private_standardDeviationOfLn = standardDeviationOfLn;
-    delete SGEXTN::SeerattraNum::UnsafeCasts<std::lognormal_distribution<FloatingPoint>>::uneraseType(private_stlDistribution);
-    private_stlDistribution = SGEXTN::SeerattraNum::UnsafeCasts<std::lognormal_distribution<FloatingPoint>>::eraseType(new std::lognormal_distribution<FloatingPoint>(private_meanOfLn, standardDeviationOfLn));
+    private_normalDistribution.setStandardDeviation(standardDeviationOfLn);
 }
-
-template class SGEXTN::SeerattraNum::LogNormalDistribution<float>;
-template class SGEXTN::SeerattraNum::LogNormalDistribution<double>;
